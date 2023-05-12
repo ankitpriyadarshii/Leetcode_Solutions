612cd676452779426075df370240b6a9c5922237
ANKIT PRIYADARSHI <72643034+ankitpriyadarshii@users.noreply.github.com>
2023-05-12 20:24:06 +0530

    Create Leetcode_MinDistToTheTargetElement.cpp

---
+++ Leetcode_MinDistToTheTargetElement.cpp
@@ -0,0 +1,23 @@
+//Minimum Distance to the Target Element
+//https://leetcode.com/problems/minimum-distance-to-the-target-element/description/
+
+
+class Solution {
+public:
+    int getMinDistance(vector<int>& a, int target, int start) {
+        int e1 = -1;
+        int dist = INT_MAX;
+        
+        for(int i = 0; i < a.size(); i++)
+        {
+            if(a[i] == target)
+                e1 = i;    
+            if(e1 != -1)
+                if(abs(e1-start) < dist)
+                    dist = abs(e1-start);
+        }
+        if(dist == INT_MAX)
+            return -1;
+        return dist;
+    }
+};
